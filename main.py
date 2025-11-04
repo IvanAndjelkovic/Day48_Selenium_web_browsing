efd5a82c385e29972433d144006fa30a26d9495d
IvanAndjelkovic <ivan.andelkovic707@gmail.com>
2025-11-04 09:15:09 +0100

    Added a  search for Selenium by class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By  
 
 # Option 1: Let Selenium locate driver automatically (Selenium 4.6+)
 chrome_options = webdriver.ChromeOptions()
@@ -9,5 +10,10 @@ driver = webdriver.Chrome(options=chrome_options)
 
 
 
-driver.get("https://www.amazon.de")
-driver.close()
+
+driver.get("https://www.amazon.de/Lenovo-Tablet-Display-MediaTek-Android/dp/B0DF2G9HK7/ref=zg_bs_g_429874031_d_sccl_1/262-0067990-5553324?th=1")
+
+price_eur = driver.find_element(By.CLASS_NAME,value="a-price-whole")
+price_cent = driver.find_element(By.CLASS_NAME,value="a-price-fraction")
+print(f"The price is {price_eur.text}.{price_cent.text}")
+driver.quit()
